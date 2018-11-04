efe28b3998a13ede74d32d30c3e1ddb0698896df
Jim Ritchey <jim0ritchey@gmail.com>
2018-11-03 21:09:14 -0400

    Added JS functionality and more basic styling, added ability to clear topic list

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
     var topics = JSON.parse(localStorage.getItem("topicList"));
-    if(topics === null) {topics = [];}
+    if(topics === null) {topics = ["snake", "dog", "science", "tech"];}
     console.log(topics);
     console.log(topics.length);
 
@@ -26,10 +26,9 @@ $(document).ready(function(){
         var topic = $(button).attr("data-topic");
         var limit = $("#limit").val();
         if(limit === ""){limit = 10;};
-        var rating = "";
+        var rating = $("#rating").val();
         var offset = $(button).attr("data-offset");
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=KG1zDRQwtw1e9SVqH91kwxeOxINgtMBG&rating="
-         + rating + "&limit=" + limit + "&offset=" + offset;
+        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + topic + "&api_key=KG1zDRQwtw1e9SVqH91kwxeOxINgtMBG&limit=" + limit + "&offset=" + offset;
 
         $.ajax({
             url: queryURL,
@@ -60,6 +59,7 @@ $(document).ready(function(){
                 $(textDiv).append(rating);
 
                 var favorite = $("<p>");
+                $(favorite).addClass("add-favorite");
                 $(favorite).text("Add to favorites");
                 $(textDiv).append(favorite);
 
@@ -92,6 +92,24 @@ $(document).ready(function(){
             $("#topic-list").prepend(newTopic);
         }
     }
+
+    $(document).on("click", ".add-favorite", function(){
+        if($(this).text() === "Add to favorites"){
+            $(this).text("Remove favorite");
+        }
+        else {
+            $(this).text("Add to favorites");
+        }
+    });
+
+    $(document).on("click", "#clear", function(){
+        if(confirm("Are you sure you want to clear your topic list?")) {
+            topics = [];
+            $("#topic-list").empty();
+            localStorage.clear();
+        }
+    });
+
     loadList();
 });
 
